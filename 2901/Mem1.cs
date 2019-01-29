f938fcf57145783cb491f75206a06a9f84e43f19
yonatan2019 <47153587+yonatan2019@users.noreply.github.com>
2019-01-29 19:11:46 +0200

    Hazara

---
+++ 2901/Mem1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2901
+{
+    class Mem1
+    {
+
+        public static int Power2(int x)
+        {
+            return x * x;
+        }
+
+        public static void Main2()
+        {
+            double d = 5.5;
+            float f = 5.5f;
+            string name = "itay";
+            Console.WriteLine("hello" + name + " !");
+            Console.WriteLine($"hello {name} !");
+
+            int x = 1; //= Convert.ToInt32( Console.ReadLine()) ;
+
+            //if (x % 2 == 0)
+            //{
+            //    Console.WriteLine($"{x} Zugi");
+            //}
+            //else
+            //{
+            //    Console.WriteLine($"{x} odd");
+            //}
+
+
+            if (x == 0)
+            {
+                Console.WriteLine("zero");
+
+            }
+            else if (x == 1)
+            {
+                Console.WriteLine("one");
+            }
+            else if (x == 2)
+            {
+                Console.WriteLine("two");
+            }
+            else
+            {
+                Console.WriteLine("not zero not one not two");
+            }
+
+            switch (x)
+            {
+                case 0:
+                    Console.WriteLine("Zero");
+                    break;
+                case 1:
+                    Console.WriteLine("One");
+                    break;
+                case 2:
+                    Console.WriteLine("two");
+                    break;
+                default:
+                    Console.WriteLine("not zero not one not two");
+                    break;
+            }
+
+            x = 0;
+            Console.WriteLine("enter number until number > 0");
+            x = Convert.ToInt32(Console.ReadLine());
+            while (x <= 0)
+            {
+                Console.WriteLine("enter number until number > 0");
+                x = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("x > 0");
+
+            do
+            {
+                Console.WriteLine("enter number until number > 0");
+                x = Convert.ToInt32(Console.ReadLine());
+            }
+            while (x <= 0);
+
+            int j = 1;
+            do
+            {
+                Console.WriteLine(j);
+                j++;
+            }
+            while (j <= 10);
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            int sum = 0;
+            for (int j2 = 0; j2 < 100; j2++)
+            {
+                int grade = Convert.ToInt32(Console.ReadLine());
+                if (grade < 0)
+                {
+                    continue;
+                }
+                sum = sum + grade;
+                if (sum > 100_000)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine($"average is {(sum * 100.0)}");
+
+            for (int i3 = 0; i3 < 10; i3++)
+            {
+                for (int i4 = 0; i4 < 10; i4++)
+                {
+                    break;
+                }
+                break;
+            }
+        }
+    }
+}
---
+++ 2901/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2901
+{
+    class Program
+    {
+
+
+        private static void GetNumbers(int a, int b, int c, double d1, float f1 = 8.8f)
+        {
+            double d = 5.5;
+            float f = 5.5f;
+            string name = "itay";
+            Console.WriteLine("hello" + name + " !");
+            Console.WriteLine($"hello {name} !");
+        }
+
+        private static int Power2(int x)
+        {
+
+            return x * x;
+        }
+
+        private static void MaxPower2(ref int z1, ref int z2)
+        {
+            z1 = z1 * z1;
+            z2 = z2 * z2;
+        }
+
+        private static void MaxPower3(out int z1, out int z2)
+        {
+            z1 = Convert.ToInt32(Console.ReadLine());
+            z1 = z1 * z1;
+
+            z2 = Convert.ToInt32(Console.ReadLine());
+            z2 = z2 * z2;
+        }
+
+        static void Main(string[] args)
+        {
+
+            int x1 = 5, x2 = 7;
+
+            MaxPower2(ref x1, ref x2);
+
+            int x3, x4;
+            MaxPower3(out x3, out x4);
+
+            Console.WriteLine($"x1 = {x1}");
+            Console.WriteLine($"x2 = {x2}");
+
+            //string st = Console.ReadLine();
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            int result = Power2(number);
+            Console.WriteLine($"{number}^2 = {result}");
+
+            int y = 4;
+            GetNumbers(5, 6, 7, 7.8);
+
+            int x = 5;
+
+            Console.WriteLine(Power2(2));
+           
+            //ClaculateAverage();
+
+            //PrintResult();
+
+            //Function1();
+
+            ////
+            ////
+            ////
+
+            //Function1();
+
+            //Function1();
+        }
+
+
+    }
+}
